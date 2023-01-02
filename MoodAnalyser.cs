e53c1dae02458f499062c7282ebefe4092bf5c21
gharjare <gharjarechetan@gmail.com>
2023-01-02 16:49:54 +0530

    [chetan gharjare]:The mood should return happy or sad

---
+++ MoodAnalyser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalizer
+{
+    public class MoodAnalyser
+    {
+        public string AnalyzeMood(string message)
+        {
+            if (message.ToLower().Contains("Sad"))
+                return "Sad";
+            return "Happy";
+        }
+        
+        
+    }
+}
